e130221beaea30993402e98844ebc3b3f829b9ea
bigmacd <martin.cooley@gmail.com>
2018-11-19 21:17:11 -0500

    initial commit for powerball checker

---
+++ powerball.py
@@ -0,0 +1,21 @@
+
+# maybe there are others?
+url = "https://www.valottery.com/SearchNumbers/powerball/"
+
+myNumbers = [
+    [ {10, 15, 19, 23, 46 }, { 3 } ],
+    [ { 2, 10, 15, 22, 23 }, { 4 } ],
+    [ { 1, 14, 11, 19, 23 }, { 1 } ],
+    [ { 2,  6,  9, 19, 27 }, { 2 } ],
+]
+
+
+checkNumbers(currentNumbers):
+    pass
+
+
+def getNumbers():
+    pass
+
+
+if __name__ == "__main__":
