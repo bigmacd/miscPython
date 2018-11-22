5deac2db1cff7caab151e2598f5e623efe675650
bigmacd <martin.cooley@gmail.com>
2018-11-22 10:00:23 -0500

    calculate prizes

--- powerball.py
+++ powerball.py
@@ -9,15 +9,35 @@ myNumbers = [
 ]
 
 
+# The key to this dictionary is whether the powerball was matched
+prizes = {
+    True:
+    {
+        0: 4,
+        1: 4,
+        2: 7, 
+        3: 100,
+        4: 50000, 
+        5: "!!!!!!!!!!!!!"
+    }, 
+    False:
+    {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 7, 
+        4: 100, 
+        5: 1000000
+    }
+}
+
+
 def checkNumbers(currentNumbers: list):
     for number, ticket in enumerate(myNumbers):
         powerballMatch = True if len(ticket[1] & currentNumbers[1]) else False
         matches = len(ticket[0] & currentNumbers[0])
-        msg = "{0} the powerball".format("and" if powerballMatch else "but not")
-        print("Ticket {0} has {1} match{2} {3}".format(number + 1, 
-                                                       matches, 
-                                                       "" if matches == 1 else "es", 
-                                                       msg))
+        print("Ticket {0}: won {1}".format(number + 1,
+                                           prizes[powerballMatch][matches]))
 
 
 def getNumbers():
