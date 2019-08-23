ad72da5c32bd15d0094674454f318601bb2fb287
Martin Cooley <martin.cooley@gmail.com>
2019-08-23 13:54:55 -0400

    trying to sleep just once

--- powerball.py
+++ powerball.py
@@ -20,18 +20,18 @@ prizes = {
     {
         0: 4,
         1: 4,
-        2: 7, 
+        2: 7,
         3: 100,
-        4: 50000, 
+        4: 50000,
         5: "!!!!!!!!!!!!!"
-    }, 
+    },
     False:
     {
         0: 0,
         1: 0,
         2: 0,
-        3: 7, 
-        4: 100, 
+        3: 7,
+        4: 100,
         5: 1000000
     }
 }
@@ -60,7 +60,7 @@ def checkNumbers(currentNumbers: list):
         powerballMatch = True if len(ticket[1] & currentNumbers[1]) else False
         matches = len(ticket[0] & currentNumbers[0])
         if powerballMatch == True:
-            Print ("Powerball matches!") 
+            Print ("Powerball matches!")
         Print ("Matched {0} numbers".format(matches))
         Print("Ticket {0}: won {1}\n".format(number + 1,
                                              prizes[powerballMatch][matches]))
@@ -78,7 +78,7 @@ def getNumbers():
     numbersPage = browser.get(url) #, verify=False)
     # The main section in which we are interested
     panel = numbersPage.soup.find("div", {"class": "right-panel"})
-    
+
     # Output the date for these numbers
     Print("The current date being checked is: {0}".format(panel.find("h3", { "class": "title-display"}).contents[0]))
 
@@ -93,7 +93,7 @@ def getNumbers():
     powerball  = numbers.find("span", { "id": "bonus-ball-display"}).contents[0]
 
     retVal = [ { int(b1), int(b2), int(b3), int(b4), int(b5)}, { int(powerball) } ]
-    
+
     # Output what we found
     #print("Current Numbers: {0} {1} {2} {3} {4} {5}".format(b1, b2, b3, b4, b5, powerball))
     printEntry("Current Numbers: {0} \t\tPowerball: {1}\n", retVal)
@@ -105,22 +105,38 @@ def timeToWakeUp():
     # if it is around just past midnight on Thursdays(3) or Sundays(6), go ahead and run
     dayOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     retVal = False
+    sleepTime = 0
     n = datetime.now()
     print("found weekday = {0}, hour is: {1}".format(dayOfWeek[n.weekday()], n.hour))
-    if n.weekday() == 3 or n.weekday() == 6:
-        if n.hour == 0:
-            retVal = True
-    return retVal
+
+    whereIam = (n.weekday() * 24) + n.hour
+    if whereIam == 72:
+        retVal = True
+        sleepTime = 72
+    elif whereIam == 144:
+        retVal = True
+        sleepTime = 96
+    elif whereIam < 72:
+        sleepTime = 72 - (whereIam + 1)
+    elif whereIam < 144:
+        sleepTime = 144 - (whereIam + 1)
+    else:
+        sleepTime = (168 - (whereIam + 1)) + 72
+    //if n.weekday() == 3 or n.weekday() == 6:
+    //    if n.hour == 0:
+    //        retVal = True
+    return retVal, sleepTime
 
 
 if __name__ == "__main__":
     while True:
         print("checking if it is time to wake up...")
-        if timeToWakeUp():
+        isTime, sleepHours = timeToWakeUp()
+        if isTime:
             outputMessage = ""
             print ("waking up!")
             checkNumbers(getNumbers())
-            
+
             g = Gmail()
             g.setFrom('martin.cooley@gmail.com')
             g.addRecipient('martin.cooley@gmail.com')
@@ -130,5 +146,5 @@ if __name__ == "__main__":
             appkey = os.environ["appkey"]
             g.setAuth(username, appkey)
             g.send()
-        print ("sleeping for one hour")
-        time.sleep(60 * 60)
+        print ("sleeping for {0} hours".format(sleepHours))
+        time.sleep(sleepHours * 60 * 60)
